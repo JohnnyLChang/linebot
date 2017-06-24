cebf96e862b99c2644de0da4ca450cc247d8703a
Johnny Chang <riversnow@gmail.com>
2017-06-24 05:16:22 -0700

    add locking for dict in heroku

--- locbot/views.py
+++ locbot/views.py
@@ -28,7 +28,10 @@ def handle_location_message(event):
         logging.debug(str(threading.currentThread().ident) + "|handle_location_message >>>")
         logging.info("current map lenght:"+str(len(usermap)))
         logging.info(usermap)
-        if event.source.user_id in usermap and usermap["event.source.user_id"]:
+        if event.source.user_id in usermap:
+            key = usermap["event.source.user_id"]
+        
+        if key:
             keyword = usermap[event.source.user_id]
             logging.info(event.source.user_id + "=> find loc:" + keyword)
             usermap["event.source.user_id"] = ""
